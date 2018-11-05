3e4f5f7f302c50f9443560010e72269a51208c05
Eric.Karuri <erukungu@abnosoftwares.co.ke>
2018-11-05 08:59:15 +0300

    initial commit

---
+++ JwtAuth/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("[action]")]
+        public JsonResult TestLog()
+        {
+            try
+            {
+                Log.Instance.Debug("We're going to throw an exception now.");
+                Log.Instance.Warn("It's gonna happen!!");
+                return new JsonResult("Hello World");
+            }
+            catch (ApplicationException ae)
+            {
+                Log.Instance.ErrorException("Error doing something...", ae);
+                return new JsonResult("Error");
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
