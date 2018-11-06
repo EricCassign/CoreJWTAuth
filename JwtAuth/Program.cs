7db03549679a6ae0fc090b7ed1a4b1e92ca7f92e
Eric.Karuri <erukungu@abnosoftwares.co.ke>
2018-11-06 11:01:02 +0300

    dont show  server name

--- JwtAuth/Program.cs
+++ JwtAuth/Program.cs
@@ -19,6 +19,10 @@ namespace JwtAuth
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                });
     }
 }
